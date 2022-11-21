6bd4e9e7a4f37a8c522d16eb3eb0fe0d8db9ef3d
Gisela <giselatb@outlook.com>
2022-11-21 23:20:12 +0100

    add search by domain

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,8 +11,8 @@ export class ServersComponent {
 
   constructor(private mastodonService: MastodonService) { }
 
-
   servers: ServerInfo[] = [];
+  cachedServers: ServerInfo[] = [];
   last_mastodon_version: string = "4.0.2";
   self = this;
 
@@ -22,42 +22,45 @@ export class ServersComponent {
   getServers(orderBy: string = 'version'): void {
     this.mastodonService.getServers().subscribe(servers => {
 
+      this.cachedServers = servers;
       switch (orderBy) {
         case 'users':
-          this.servers = servers.sort(function (a: ServerInfo, b: ServerInfo) {
+          this.servers = this.cachedServers.sort(function (a: ServerInfo, b: ServerInfo) {
             return b.total_users - a.total_users;
           });
           break;
         case 'usersByweek':
-          this.servers = servers.sort(function (a: ServerInfo, b: ServerInfo) {
+          this.servers = this.cachedServers.sort(function (a: ServerInfo, b: ServerInfo) {
             return b.last_week_users - a.last_week_users;
           });
           break;
         case 'version':
           //Get versions
           let versionOrder = ["4.0.2", "4.0.2+uri1.13", "4.0.1", "4.0.2+glitch", "4.0.0rc2+glitch", "4.0.0rc2", "3.5.5", "3.5.3", "3.5.3~wxw", "3.5.3+glitch", "3.5.3~island", "3.5.1", "3.4.6", "3.4.5", "3.4.1", "3.3.2", "3.2.0+glitch", "3.1.5", "3.1.3"];
-          this.servers = servers.sort((a, b) => versionOrder.indexOf(a.version) - versionOrder.indexOf(b.version));
+          this.servers = this.cachedServers.sort((a, b) => versionOrder.indexOf(a.version) - versionOrder.indexOf(b.version));
           break;
         default:
-          this.servers = servers;
+          this.servers = this.cachedServers;
           break;
       }
 
 
     });
+  }
 
-    // switch (orderBy) {
-    //   case 'users':
-    //     console.log('Sort by users');
-    //     let sortedServers = this.
+  searchByDomain(e: any): void {
 
-    //     console.log(sortedServers);
+    let domainName: string = e.target.value;
 
-    //     break;
+    if (domainName.trim() == '') {
+      this.servers = this.cachedServers;
 
-    //   default:
-    //     break;
-    // }
+    }
+    else {
+
+      console.log(`Looking for ${domainName}`);
+      this.servers = this.cachedServers.filter(({ domain }) => domain.startsWith(domainName));
+    }
 
   }
 
